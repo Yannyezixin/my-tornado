06a0142db7b412d392704255012b4bbe90d89af3
Yannyezixin <646840080@qq.com>
2015-08-07 11:03:28 +0800

    handlers and post method

--- hello/hello.py
+++ hello/hello.py
@@ -1,3 +1,4 @@
+import textwrap
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -11,10 +12,26 @@ class IndexHandler(tornado.web.RequestHandler):
         greeting = self.get_argument('greeting', 'hello')
         self.write(greeting + ', friendly user!')
 
+class ReverseHandler(tornado.web.RequestHandler):
+    def get(self, input):
+        self.write(input[::-1])
+
+class WrapHandler(tornado.web.RequestHandler):
+    def post(self):
+        text = self.get_argument('text', 'text')
+        width = self.get_argument('width', 2)
+        self.write(textwrap.fill(text, int(width)))
+
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    app = tornado.web.Application(
+        handlers = [
+            (r"/index", IndexHandler),
+            (r"/reverse/(\w+)", ReverseHandler),
+            (r"/wrap", WrapHandler)
+        ]
+    )
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
