d20fbe7a34bd03655a4330940870787ec83f3041
Yannyezixin <646840080@qq.com>
2015-08-14 12:41:41 +0800

    添加protobuf逻辑

--- protobuf/app/config/params.py
+++ protobuf/app/config/params.py
@@ -6,9 +6,12 @@ import os
 params = {
     'app_dir_name': 'app',
     'app_root': os.path.abspath('..'),
+    'data_dir': os.path.join(os.path.abspath('.'), 'app', 'data'),
+    'data_file': os.path.join(os.path.abspath('.'), 'app', 'data', 'proto.data'),
 }
 
 settings = {
     'template_path': os.path.join(os.path.abspath('.'), params['app_dir_name'], 'views'),
     'static_path': os.path.join(os.path.abspath('.'), params['app_dir_name'], 'assets'),
 }
+
--- protobuf/app/controller/Person.py
+++ protobuf/app/controller/Person.py
@@ -1,5 +1,54 @@
 import tornado.web
+import person_pb2 as API
 
 class PersonHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('helloworld.html')
+        address_book = API.AddressBook()
+        try:
+            f = open(self.application.config['data_file'], "rb")
+            address_book.ParseFromString(f.read())
+            f.close()
+        except IOError:
+            print 'could not open file'
+
+        persons = address_book.person
+
+        pd = API
+        self.render('list.html', persons = persons, pd = pd);
+
+    def post(self):
+        address_book = API.AddressBook()
+
+        try:
+            f = open(self.application.config['data_file'], "rb")
+            address_book.ParseFromString(f.read())
+            f.close()
+        except IOError:
+            print 'could not open file'
+
+        person = address_book.person.add()
+        person.id = int(self.get_argument('id', 0))
+        person.name = self.get_argument('name', 'fuck')
+        person.email = self.get_argument('email', 'null')
+        number = self.get_argument('number')
+        type = self.get_argument('type')
+        phone_number = person.phone.add()
+        phone_number.number = number
+
+        if type == "mobile":
+            phone_number.type = API.Person.MOBILE
+        elif type == "home":
+            phone_number.type = API.Person.HOME
+        elif type == "work":
+            phone_number.type = API.Person.WORK
+        else:
+            self.write('something error')
+
+        try:
+            f = open(self.application.config['data_file'], "wb")
+            content = address_book.SerializeToString()
+            f.write(content)
+            f.close()
+        except IOError:
+            print 'can not write into file'
+
